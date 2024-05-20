0a6ae40592eac5ee732a2cc1c533317c572e3fe3
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-20 22:26:49 +0300

    Update looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def happy_new_year():
-    # code goes here!
+    x = 0
+    while x <= 9:
+        print(10 - x)
+        x += 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    all_squares = [integer * integer for integer in int_list]
+    return all_squares
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for x in range(1, 101):
+      if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+      elif x % 3 == 0:
+        print("Fizz")
+      elif x % 5 == 0:
+        print("Buzz")
+      else:
+        print(x)
     pass
+
+fizzbuzz()
